ed6d01cc45f290198519e8ac538886915e712dbd
Matheus Bodo <mcbodo@gmail.com>
2016-07-25 21:41:19 -0300

    creating simple hello world application

---
+++ 01-simple-spring-boot-application/src/main/java/br/com/matheusbodo/simple/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.matheusbodo.simple;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/")
+	public String hello() {
+		return "hello";
+	}
+}
